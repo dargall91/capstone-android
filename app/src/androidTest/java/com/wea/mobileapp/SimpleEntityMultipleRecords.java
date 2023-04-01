324fa0f3d2c39b4df7e26552e27b5f81010ce85c
Alex Klein <akendo21@gmail.com>
2023-04-01 13:44:17 -0500

    new test

---
+++ app/src/androidTest/java/com/wea/mobileapp/SimpleEntityMultipleRecords.java
@@ -0,0 +1,43 @@
+package com.wea.mobileapp;
+
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import com.wea.local.DBHandler;
+import com.wea.models.CMACMessage;
+import com.wea.models.CollectedDeviceData;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class SimpleEntityMultipleRecords {
+    private DBHandler dbHandler;
+
+    @Before
+    public void createDB() {
+        Context context = ApplicationProvider.getApplicationContext();
+        dbHandler = new DBHandler(context);
+    }
+
+    @After
+    public void closeDb() throws IOException {
+        dbHandler.close();
+    }
+
+    @Test
+    public void multipleDatabaseEntry() throws Exception {
+        CMACMessage cmac = new CMACMessage();
+        CMACMessage cmac_two = new CMACMessage();
+        cmac.setMessageNumber("123");
+        cmac_two.setMessageNumber("456");
+        CollectedDeviceData data = new CollectedDeviceData(cmac, false, false);
+        CollectedDeviceData data2 = new CollectedDeviceData(cmac_two, false, false);
+        dbHandler.addNewRecord(data, "test");
+        dbHandler.addNewRecord(data2, "test2");
+        assert dbHandler.getAllRows().size() == 2;
+    }
+}
